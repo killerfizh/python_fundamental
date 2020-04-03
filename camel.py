281bee41be40f7e4f9e399ef8badc2ec1141d7ac
Krishna Kompalli <31897761+killerfizh@users.noreply.github.com>
2020-04-03 12:14:49 +0530

    Create camel.py

---
+++ camel.py
@@ -0,0 +1,14 @@
+def Camel(s): 
+    if(len(s) == 0): 
+        return
+    s1 = '' 
+    s1 += s[0].upper() 
+    for i in range(1, len(s) - 1): 
+        if (s[i] == ' '): 
+            s1 += s[i + 1].upper() 
+            i += 1
+        elif(s[i - 1] != ' '): 
+            s1 += s[i]  
+    print(s1)   
+s = str(input())
+Camel(s)
